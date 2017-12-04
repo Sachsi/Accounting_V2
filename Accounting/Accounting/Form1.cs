dd08894944ad0e38e05dceefb48619afb8152dba
Tobias, Sachse <tobisachse27@msn.com>
2017-12-04 21:50:41 +0100

    created Income, Expenses and Custoemr tab with Reportviewer, Buttons, Textboxes and Labels

--- Accounting/Accounting/Form1.cs
+++ Accounting/Accounting/Form1.cs
@@ -17,5 +17,11 @@ namespace Accounting
             InitializeComponent();
             
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            this.reportViewer1.RefreshReport();
+        }
     }
 }
