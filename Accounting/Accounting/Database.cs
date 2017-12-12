2f044c5db1243e544ac665bb44645407b27aa5c4
Tobias, Sachse <tobisachse27@msn.com>
2017-12-12 21:38:21 +0100

    Create my first SQL Table by Code First then Model

---
+++ Accounting/Accounting/Database.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace Accounting
+{
+    public class Database
+    {
+        public class AccountingDatabase : DbContext
+        {
+            public DbSet<Customer> Customers { get; set; }
+        }
+
+        public class Customer
+        {
+            public  int ID { get; set; }
+            public string FullName { get; set; }
+        }
+    }
+}
