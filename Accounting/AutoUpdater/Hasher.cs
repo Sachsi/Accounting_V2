01f49f6a862ca8356a81231fda11a179efc69a7f
Tobias, Sachse <tobisachse27@msn.com>
2018-02-18 16:38:34 +0100

    Update Form hinzugefügt.

---
+++ Accounting/AutoUpdater/Hasher.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace AutoUpdater
+{
+    /// <summary>
+    /// the type of Hash to create
+    /// </summary>
+    internal enum HashType
+    {
+        MD5,
+        SHA1,
+        SHA512
+    }
+    /// <summary>
+    /// class used  the generate hash sums of the files
+    /// </summary>
+    internal static class Hasher
+    {
+        /// <summary>
+        /// gernerate a hash sum of the file
+        /// </summary>
+        /// <param name="filePath">the file to hash</param>
+        /// <param name="algo">the type of hash</param>
+        /// <returns>the computed hash</returns>
+        internal static string Hashfile(string filePath, HashType algo)
+        {
+            switch (algo)
+            {
+                case HashType.MD5:
+                    return MakeHashString(MD5.Create().ComputeHash(new FileStream(filePath, FileMode.Open)));
+                case HashType.SHA1:
+                    return MakeHashString(SHA1.Create().ComputeHash(new FileStream(filePath, FileMode.Open)));
+                case HashType.SHA512:
+                    return MakeHashString(SHA512.Create().ComputeHash(new FileStream(filePath, FileMode.Open)));
+                default:
+                    return "";
+            }
+        }
+        /// <summary>
+        /// converts byte[] to string
+        /// </summary>
+        /// <param name="hash">the hash to convert</param>
+        /// <returns>hash as string</returns>
+        private static string MakeHashString(byte[] hash)
+        {
+            StringBuilder s = new StringBuilder(hash.Length * 2);
+
+            foreach (byte b in hash)
+            {
+                s.Append(b.ToString("X2").ToLower());
+            }
+            return s.ToString();
+        }
+    }
+}
---
+++ Accounting/AutoUpdater/IAutoUpdater.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace AutoUpdater
+{
+    public interface IAutoUpdater
+    {
+        /// <summary>
+        /// Name der Anwendung
+        /// </summary>
+        string ApplicationName { get; }
+        /// <summary>
+        /// ID der Anwendung
+        /// </summary>
+        string ApplicationID { get; }
+        /// <summary>
+        /// Versionsinformationen der Anwendung
+        /// </summary>
+        Assembly ApplicationAssembly { get; }
+        /// <summary>
+        /// Icon der Anwendung
+        /// </summary>
+        Icon ApplicationIcon { get; }
+        /// <summary>
+        /// Ort von wo die Updates heruntergeladen werden
+        /// </summary>
+        Uri UpdateXmlLocation { get; }  
+        /// <summary>
+        /// Der Context von einem Programm
+        /// Windows Form Anwendungen benutzen, "this"
+        /// </summary>
+        Form Context { get; }
+    }
+}
